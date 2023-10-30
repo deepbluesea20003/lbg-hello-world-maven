2ce4458e47fe6d00d750b05c7fb4050600d617f6
Adam.B3 <readingfootyclub1871@gmail.com>
2023-10-30 15:48:49 +0000

    deleted a name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,7 +12,6 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Adam"));
         System.out.println(sayGoodbye());
     }
